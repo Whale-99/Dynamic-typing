8cf60bd7bcbacc40a2b8ac87d0ce9e71eee55b26
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-04-28 23:23:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Никита"
+print("Имя: " + name)
+age = "24"
+print("Возраст: " + age)
+age = 24
+NextYearAge = age + 1
+print("Возраст через год: " + str(NextYearAge))
+in_student = True
+print("Студент: " + str(in_student))
